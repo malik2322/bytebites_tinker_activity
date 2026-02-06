7bc17f0f53c5f88f327b13ddbb27e10e5489581c
Asad <dasamalik18@gmail.com>
2026-02-06 01:09:20 -0600

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,7 @@
+# The system consists of four core entities: User, Restaurant, Order, and Driver.
+# The User initiates orders and stores personal, address, and payment information.
+# The Restaurant provides food, maintains operational details, and manages a menu catalog.
+# The Order acts as the central transaction, connecting the User, Restaurant, and Driver while tracking order status, contents, and total cost.
+# The Driver handles delivery logistics, including availability, location tracking, and vehicle details.
+
+
